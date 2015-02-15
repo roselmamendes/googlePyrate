31c763ec745e8b47e186f761d40f647349c27812
Roselma Mendes <rmendes@thoughtworks.com>
2015-02-15 20:15:39 -0300

    Set up environment for use PEP8 check.

---
+++ run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo '---------------------------Running PEP8---------------------------'
+for f in $(find ./googleResults -name '*.py' -or -name '*.doc'); do pep8 $f; done
+echo '------------------------------------------------------------------'
+echo '---------------------Running PEP8 OVER TESTS----------------------'
+for f in $(find ./tests -name '*.py' -or -name '*.doc'); do pep8 $f; done
+echo '------------------------------------------------------------------'
+echo '---------------------------Running Tests--------------------------'
+python3 -m unittest

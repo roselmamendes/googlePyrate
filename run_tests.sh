51fe7951af0ade8c8a1772023b47e831638eab05
Roselma Mendes <rmendes@thoughtworks.com>
2015-02-15 20:22:06 -0300

    Updating run_tests.sh.

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+echo '---------------------------Running Tests--------------------------'
+python3 -m unittest
 echo '---------------------------Running PEP8---------------------------'
 for f in $(find ./googleResults -name '*.py' -or -name '*.doc'); do pep8 $f; done
 echo '------------------------------------------------------------------'
 echo '---------------------Running PEP8 OVER TESTS----------------------'
 for f in $(find ./tests -name '*.py' -or -name '*.doc'); do pep8 $f; done
-echo '------------------------------------------------------------------'
-echo '---------------------------Running Tests--------------------------'
-python3 -m unittest

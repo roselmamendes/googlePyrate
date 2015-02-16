97a20603aec533d3e111baad9f8e73b74fcd2f23
Roselma Mendes <rmendes@thoughtworks.com>
2015-02-16 20:48:08 -0300

    Changed the strategy. It's no going use web crawler behaviour but google search rest.

--- googleResults/google_crawler.py
+++ googleResults/google_crawler.py
@@ -1,17 +1,5 @@
-from urllib.request import urlopen
-
-
 class GoogleCrawler(object):
 
-    def __init__(self, url):
-        self.url = url
-
-    def getresponse(self):
-        return urlopen(self.url)
-
-    def crawl(self, forsearch):
-        pass
-
     def buildgoogleresults(self, tosearch):
-        forsearch = tosearch.replace(' ', '+')
-        self.crawl(forsearch)
+        self.forsearch = tosearch.replace(' ', '+')
+        return self.forsearch
--- tests/test_google_crawler.py
+++ tests/test_google_crawler.py
@@ -1,22 +1,10 @@
-from unittest import TestCase, mock
+from unittest import TestCase
 from googleResults.google_crawler import GoogleCrawler
 
 
 class GoogleCrawlerTest(TestCase):
 
-    def test_should_receive_a_url_then_return_response(self):
-        url = 'http://www.codingdojo.org'
-        google_crawler = GoogleCrawler(url)
-
-        self.assertIsNotNone(google_crawler.getresponse())
-
     def test_should_a_search_with_more_than_one_word_with_plus_between_the_words(self):
-        url = 'http://www.codingdojo.org'
-        google_crawler = GoogleCrawler(url)
+        google_crawler = GoogleCrawler()
         tosearch = "python crawler"
-
-        google_crawler.crawl = mock.Mock()
-
-        google_crawler.buildgoogleresults(tosearch)
-
-        google_crawler.crawl.assert_called_once_with("python+crawler")
+        self.assertEqual('python+crawler', google_crawler.buildgoogleresults(tosearch))

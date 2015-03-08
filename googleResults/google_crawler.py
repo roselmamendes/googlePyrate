9f10796a36d070b277af09ec61dc3b0c2eeff2dd
Roselma Mendes <rmendes@thoughtworks.com>
2015-03-08 20:28:47 -0300

    Used PyQuery and Requests for run searchs from Google.

--- googleResults/google_crawler.py
+++ googleResults/google_crawler.py
@@ -1,5 +1,15 @@
+from pyquery import PyQuery
+import requests
+
 class GoogleCrawler(object):
 
     def buildgoogleresults(self, tosearch):
         self.forsearch = tosearch.replace(' ', '+')
         return self.forsearch
+
+    def call(self, url):
+        r = requests.get(url)
+        d = PyQuery(r.text)
+        if d:
+            print(d)
+        return r.status_code
--- tests/test_google_crawler.py
+++ tests/test_google_crawler.py
@@ -8,3 +8,11 @@ class GoogleCrawlerTest(TestCase):
         google_crawler = GoogleCrawler()
         tosearch = "python crawler"
         self.assertEqual('python+crawler', google_crawler.buildgoogleresults(tosearch))
+
+    def test_call_a_google_url_and_get_201(self):
+        google_crawler = GoogleCrawler()
+        self.assertEqual(200, google_crawler.call('http://www.google.com/search?q=teste'))
+
+    def test_if_there_is_a_problem_calling_a_google_page_return_a_status_code_different_than_200(self):
+        google_crawler = GoogleCrawler()
+        self.assertNotEqual(200, google_crawler.call('http://www.google.com/teste'))

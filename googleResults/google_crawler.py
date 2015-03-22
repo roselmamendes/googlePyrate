62fb17b819851ed8f9a18d61886423504332c4e2
Roselma Mendes <roselma.mendes@gmail.com>
2015-03-22 20:15:42 -0300

    Now it's possible read a html page and return results.

--- googleResults/google_crawler.py
+++ googleResults/google_crawler.py
@@ -18,5 +18,20 @@ class GoogleCrawler(object):
 
     def buildResults(self, html):
         d = PyQuery(html)
+        # //*[@id="rso"]/div[2]/li[1]/div/h3/a
+        # //*[@id="rso"]/div[2]
+        ol_tag = d('#rso')
 
-        return []
+        div_tag = ol_tag('div').eq(1)
+
+        a_tag = div_tag('li div h3 a')
+
+        results = []
+
+        for a in range(0, len(a_tag)):
+            results.append({
+                '<a>': a_tag[a].text,
+                'href': a_tag[a].get('href')
+                })
+
+        return results
--- tests/load_html_page.py
+++ tests/load_html_page.py
@@ -2,6 +2,7 @@ searches = {
     'python crawler': 'tests/searches/python_crawler_search.html'
 }
 
+
 def load_html(search):
     html_file = open(searches.get(search), 'r')
     html_content = html_file.read()

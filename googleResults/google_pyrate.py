6365bd18942ea234302d2f514a2c90806e99dd37
Roselma Mendes <roselma.mendes@gmail.com>
2015-03-22 21:02:13 -0300

    Started to  implement console interface.

---
+++ googleResults/google_pyrate.py
@@ -0,0 +1,21 @@
+from googleResults.google_crawler import GoogleCrawler
+
+
+class GooglePyrate:
+
+    @staticmethod
+    def welcome_message():
+        return 'Welcome to GooglePyrate!'
+
+    @staticmethod
+    def waiting_for_input_message():
+        return 'What Do You Look for? Type here ->'
+
+    @staticmethod
+    def waiting_for_input():
+        pass
+
+    @staticmethod
+    def show_the_results():
+        google_crawler = GoogleCrawler()
+        google_crawler.search('')
---
+++ tests/test_console.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from googleResults.google_pyrate import GooglePyrate
+
+
+class ConsoleTest(TestCase):
+
+    def test_should_return_welcome_message(self):
+        self.assertEqual('Welcome to GooglePyrate!', GooglePyrate.welcome_message())
+
+    def test_should_return_start_message_waiting_for_input(self):
+        self.assertEqual('What Do You Look for? Type here ->', GooglePyrate.waiting_for_input_message())
+
+    # TODO: fix it
+    def xtest_should_get_what_user_type_and_call_google_crawler_search(self):
+        google_crawler = GooglePyrate()
+        google_crawler.search = Mock()
+
+        GooglePyrate.show_the_results()
+
+        google_crawler.search.assert_called_once_with('python crawler')
